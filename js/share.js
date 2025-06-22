806daf1db5dc275aec428fbd64888a53befa0981
21r21a12b7 <21r21a12b7@mlrinstitutions.ac.in>
2025-06-22 13:33:16 +0530

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,7 +1,9 @@
 function shareApp() {
   const message = `Hello!!
 Now you can track your Institution buses' location 24/7 — from anywhere! 🚍
-🔗 Visit: https://sreejaschool-whereismybus.vercel.app`;
+🔗 Visit: https://sreejaschool-whereismybus.vercel.app
+👤 Username: Sreeja@2023
+🔒 Password: Sreeja@2023`;
   const encodedMessage = encodeURIComponent(message);
   const whatsappUrl = `https://wa.me/?text=${encodedMessage}`;
   window.open(whatsappUrl, '_blank');
